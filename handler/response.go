39836a6b2fb0713a95578ea34bcb0f3b38e5bbda
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-06-16 21:44:03 -0300

    feat: Employee Response added

---
+++ handler/response.go
@@ -0,0 +1,34 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type EmployeeResponse struct {
+    EmpID    int       `json:"emp_id"`
+    FirstName string    `json:"first_name"`
+    LastName string     `json:"last_name"`
+    BirthDay *time.Time `json:"birthday,omitempty"`
+    Sex      string     `json:"sex"`
+    Salary   int        `json:"salary"`
+}
+
+func sendSuccess(ctx *gin.Context, op string, data interface{}){
+	ctx.Header("Content-type", "application/json")
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": fmt.Sprintf("operation %s sucessfull", op),
+		"data": data,
+	})
+}
+
+func sendError(ctx *gin.Context, code int, msg string){
+	ctx.Header("Content-type", "application/json")
+	ctx.JSON(code, gin.H{
+		"message": msg,
+		"error_code": code,
+	})
+}
